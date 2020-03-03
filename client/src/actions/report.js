9935d8634f6cb4d2c12ba44f4a8ae5aad39f8ef5
ruzny <ruzny@ruzny.com>
2020-03-03 14:45:28 +0530

    working on redux

--- client/src/actions/report.js
+++ client/src/actions/report.js
@@ -1,16 +1,54 @@
 import axios from 'axios';
 import {
-  LOADING,
   GET_CONFIRMED_DATA,
   GET_DEATH_DATA,
   GET_RECOVERED_DATA
 } from './types';
 
 // GET ALL CONFIRMED DATA
-export const getConfirmedData = () => async dispatch => {};
+export const getConfirmedData = () => async dispatch => {
+  const config = {
+    header: {
+      'Content-Types': 'application/json'
+    }
+  };
+
+  try {
+    const { data } = await axios.get('/api/covid-19/confirmed', config);
+    dispatch({ type: GET_CONFIRMED_DATA, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // GET ALL DEATH DATA
-export const getDeathData = () => async dispatch => {};
+export const getDeathData = () => async dispatch => {
+  const config = {
+    header: {
+      'Content-Types': 'application/json'
+    }
+  };
+
+  try {
+    const { data } = await axios.get('/api/covid-19/deaths', config);
+    dispatch({ type: GET_DEATH_DATA, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 // GET ALL RECOVERED DATA
-export const getRecoveredData = () => async dispatch => {};
+export const getRecoveredData = () => async dispatch => {
+  const config = {
+    header: {
+      'Content-Types': 'application/json'
+    }
+  };
+
+  try {
+    const { data } = await axios.get('/api/covid-19/recovered', config);
+    dispatch({ type: GET_RECOVERED_DATA, payload: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- client/src/components/CoronavirusConfirmed.js
+++ client/src/components/CoronavirusConfirmed.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const CoronavirusConfirmed = () => {
+// REDUX
+import { connect } from 'react-redux';
+import { getConfirmedData } from '../actions/report';
+
+const CoronavirusConfirmed = ({
+  report: { confirmedData, loading },
+  getConfirmedData
+}) => {
+  useEffect(() => {
+    getConfirmedData();
+  }, [getConfirmedData]);
+
+  const content = loading ? (
+    <h2>Loading...</h2>
+  ) : (
+    confirmedData.map(data => <p>{data['Province/State']}</p>)
+  );
+
   return (
     <div>
       <h1>Coronavirus Confirmed Cases</h1>
+      <br />
+      <br />
+      {content}
     </div>
   );
 };
 
-export default CoronavirusConfirmed;
+CoronavirusConfirmed.propTypes = {
+  report: PropTypes.object.isRequired,
+  getConfirmedData: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  report: state.report
+});
+
+export default connect(mapStateToProps, { getConfirmedData })(
+  CoronavirusConfirmed
+);
