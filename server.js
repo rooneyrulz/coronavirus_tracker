dff3d52759961835f4d64264d3ac552d22977657
ruzny <ruzny@ruzny.com>
2020-03-04 22:08:44 +0530

    deploy to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { createServer } from 'http';
 import logger from 'morgan';
 import { config } from 'dotenv';
+import path from 'path';
 import 'colors';
 
 import reportRoute from './routes/report';
@@ -12,7 +13,13 @@ const server = createServer(app);
 config({ path: '.env' });
 app.use(logger('dev'));
 
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
 app.use('/api/covid-19', reportRoute);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 server.listen(process.env.PORT || 8000, () =>
   console.log(
