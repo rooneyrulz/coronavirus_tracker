b90cf8cb7e7efecb467b28385c4bdc55a519ee71
ruzny <ruzny@ruzny.com>
2020-03-09 15:38:27 +0530

    working on charts

--- client/src/pages/Chart.js
+++ client/src/pages/Chart.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import SwipeableViews from 'react-swipeable-views';
+
+// MATERIAL COMPONENTS
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+
+// REDUX
+import { connect } from 'react-redux';
+import {
+  getConfirmedData,
+  getDeathData,
+  getRecoveredData
+} from '../actions/report';
+
+// COMPONENTS
+import Spinner from '../layouts/Spinner';
 
 const TabPanel = props => {
   const { children, value, index, ...other } = props;
@@ -46,14 +59,74 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const Chart = () => {
+const Chart = ({
+  report: { confirmedData, deathData, recoveredData, loading },
+  getConfirmedData,
+  getDeathData,
+  getRecoveredData
+}) => {
+  const [chartData, setChartData] = useState([]);
+  const [filteredConfirmedData, setFilteredConfirmedData] = useState([]);
+
+  useEffect(() => {
+    getConfirmedData();
+    // getDeathData();
+    // getRecoveredData();
+
+    const filterData = confirmedData.filter(data => {
+      delete data['Province/State'];
+      delete data['Country/Region'];
+      delete data['Lat'];
+      delete data['Long'];
+      return data;
+    });
+
+    // setFilteredConfirmedData(() =>
+    //   loading
+    //     ? []
+    //     : [
+    //         ...confirmedData.filter(data => {
+    //           delete data['Province/State'];
+    //           delete data['Country/Region'];
+    //           delete data['Lat'];
+    //           delete data['Long'];
+    //           return data;
+    //         })
+    //       ]
+    // );
+
+    let total = 0;
+    let count = 0;
+
+    const mapped = filterData.map(obj => {
+      if (
+        obj[Object.keys(obj)[count]] !== null ||
+        obj[Object.keys(obj)[count]] !== undefined
+      ) {
+        // console.log(obj[Object.keys(obj)[0]]);
+        total += parseFloat(obj[Object.keys(obj)[count]]);
+
+        setChartData(() => [...chartData]);
+        count++;
+      }
+    });
+
+    console.log(total);
+
+    // fildatterData.forEach((val, key) => {
+    //   // totalCount += Number(val[Object.keys(val)[key]]);
+    //   console.log(Number(val[Object.keys(val)[key]]));
+    // });
+    // setChartData(() => (loading ? [] : [...chartData, totalCount]));
+  }, [getConfirmedData, loading]);
+
   const classes = useStyles({
     root: {
       flexGrow: 1
     }
   });
   const theme = useTheme();
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -79,23 +152,42 @@ const Chart = () => {
           <Tab label='Recovered Cases' {...a11yProps(2)} />
         </Tabs>
       </AppBar>
-      <SwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      >
-        <TabPanel value={value} index={0} dir={theme.direction}>
-          Confirmed Cases
-        </TabPanel>
-        <TabPanel value={value} index={1} dir={theme.direction}>
-          Death Tolls
-        </TabPanel>
-        <TabPanel value={value} index={2} dir={theme.direction}>
-          Recovered Cases
-        </TabPanel>
-      </SwipeableViews>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <SwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={value}
+          onChangeIndex={handleChangeIndex}
+        >
+          <TabPanel value={value} index={0} dir={theme.direction}>
+            Confirmed Cases {chartData}
+          </TabPanel>
+          <TabPanel value={value} index={1} dir={theme.direction}>
+            Death Tolls
+          </TabPanel>
+          <TabPanel value={value} index={2} dir={theme.direction}>
+            Recovered Cases
+          </TabPanel>
+        </SwipeableViews>
+      )}
     </div>
   );
 };
 
-export default Chart;
+Chart.propTypes = {
+  report: PropTypes.object.isRequired,
+  getConfirmedData: PropTypes.func.isRequired,
+  getDeathData: PropTypes.func.isRequired,
+  getRecoveredData: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  report: state.report
+});
+
+export default connect(mapStateToProps, {
+  getConfirmedData,
+  getDeathData,
+  getRecoveredData
+})(Chart);
